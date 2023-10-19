e920820d3e4502f7cb834c4e131dc474cfa2cb5d
Lalu-Mahato <135944020+Lalu-Mahato@users.noreply.github.com>
2023-10-19 10:15:15 +0530

    Initial project setup

---
+++ src/main/java/com/example/springbootsftpsample/SpringBootSftpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsftpsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSftpSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSftpSampleApplication.class, args);
+	}
+
+}
